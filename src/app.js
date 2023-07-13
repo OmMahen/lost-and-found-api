33c000031c30f314d9f8a3a1933eb7dce1d7bc46
OmMahen <102027819+OmMahen@users.noreply.github.com>
2023-07-13 15:00:15 +0800

    Remove Unnecessary Code

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Express from "express";
 import multer from "multer";
 import cors from "cors";
 
-import { getLostItem, getLostItems,  createLostItem, insertImage, getFoundItems, getFoundItem, createFoundItem, getAllItems, getItemById, createItem} from "./database.js";
+import { insertImage, getAllItems, getItemById, createItem} from "./database.js";
 const app = Express();
 
 const storage = multer.memoryStorage();
@@ -15,90 +15,14 @@ app.get("/", (req, res) => {
     res.send("Lost and Found Finder API");
 });
 
-app.get('/lost-items', async (req, res) => {
-    const items = await getLostItems();
-    res.send(items);
-});
-
-app.get('/found-items', async (req, res) => {
-    const items = await getFoundItems();
-    res.send(items);
-});
-
 app.get('/items', async (req, res) => {
     const items = await getAllItems();
     res.send(items);
 });
 
-app.get('/lost-items/:id', async (req, res) => {
-    const id = req.params.id;
-    const item = await getLostItem(id);
-    res.send(item);
-});
-
-app.get('/found-items/:id', async (req, res) => {
-    const id = req.params.id;
-    const item = await getFoundItem(id);
-    res.send(item);
-});
-
 app.get('/items/:id', async (req, res) => {
     const id = req.params.id;
     const item = await getItemById(id);
-    res.send(item);
-});
-
-app.post('/lost-items', async (req, res) => {
-    const {
-        user_name,
-        user_email,
-        user_phone,
-        item_name,
-        item_description,
-        loss_date,
-        loss_location,
-        iditem_image
-    } = req.body;
-    
-    const userData = {
-        user_name,
-        user_email,
-        user_phone,
-        item_name,
-        item_description,
-        loss_date: new Date(loss_date),
-        loss_location,
-        iditem_image
-    };
-
-    const item = await createLostItem(userData);
-    res.send(item);
-});
-
-app.post('/found-items', async (req, res) => {
-    const {
-        user_name,
-        user_email,
-        user_phone,
-        item_name,
-        item_description,
-        found_date,
-        found_location,
-        iditem_image
-    } = req.body;
-    
-    const userData = {
-        user_name,
-        user_email,
-        user_phone,
-        item_name,
-        item_description,
-        found_date: new Date(found_date),
-        found_location,
-        iditem_image
-    };
-
-    const item = await createFoundItem(userData);
     res.send(item);
 });
 
--- src/database.js
+++ src/database.js
@@ -12,56 +12,14 @@ const pool = new Pool({
   },
 });
 
-const getLostItems = async () => {
-  const { rows } = await pool.query('SELECT * FROM lost_items');
-  return rows;
-};
-
-const getFoundItems = async () => {
-  const { rows } = await pool.query('SELECT * FROM found_items');
-  return rows;
-};
-
 const getAllItems = async () => {
   const { rows } = await pool.query('SELECT iditem_image, item_location, id, item_name, item_date, item_description, status FROM items');
   return rows;
 };
 
-const getLostItem = async (id) => {
-  const { rows } = await pool.query('SELECT * FROM lost_items WHERE id = $1', [id]);
-  return rows[0];
-};
-
-const getFoundItem = async (id) => {
-  const { rows } = await pool.query('SELECT * FROM found_items WHERE id = $1', [id]);
-  return rows[0];
-};
-
 const getItemById = async (id) => {
   const { rows } = await pool.query('SELECT * FROM items WHERE id = $1', [id]);
   return rows[0];
-};
-
-const createLostItem = async (userData) => {
-  const { user_name, user_email, user_phone, item_name, item_description, loss_date, loss_location, iditem_image } = userData;
-  const { rows } = await pool.query(`
-    INSERT INTO lost_items (user_name, user_email, user_phone, item_name, item_description, loss_date, loss_location, iditem_image)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    RETURNING id
-  `, [user_name, user_email, user_phone, item_name, item_description, loss_date, loss_location, iditem_image]);
-  const id = rows[0].id;
-  return getLostItem(id);
-};
-
-const createFoundItem = async (userData) => {
-  const { user_name, user_email, user_phone, item_name, item_description, found_date, found_location, iditem_image } = userData;
-  const { rows } = await pool.query(`
-    INSERT INTO found_items (user_name, user_email, user_phone, item_name, item_description, found_date, found_location, iditem_image)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    RETURNING id
-  `, [user_name, user_email, user_phone, item_name, item_description, found_date, found_location, iditem_image]);
-  const id = rows[0].id;
-  return getFoundItem(id);
 };
 
 const createItem = async (userData) => {
@@ -85,4 +43,4 @@ const insertImage = async (image) => {
   return id;
 };
 
-export { getLostItems, createLostItem, getLostItem, insertImage, getFoundItems, getFoundItem, createFoundItem, getAllItems, getItemById, createItem };
+export { insertImage, getAllItems, getItemById, createItem };
